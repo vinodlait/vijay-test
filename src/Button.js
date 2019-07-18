21dbcdc6615abe3b441f274eeb8492351974957b
vijay <vijay.kumar@laitkor.com>
2019-07-18 12:52:05 +0530

    4

---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return <Button color="red" />;
+  }
+}
+
+export default Button; // Don’t forget to use export default!
